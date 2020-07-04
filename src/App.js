a1cbd7e5ae4ec4494bc1c3ca419d2641c8bbca25
onewithclouds <onewithclouds@protonmail.ch>
2020-07-04 16:40:49 +0300

    App.js updates to functions, work in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const numRows = 50;
+const numCols = 50;
+
+const App = () => {
+    
+    const [grid, setGrid] = useState( () => {
+	const gridInit = [];
+
+	for (let i = 0; i < numRows; i++) {
+	    console.log(gridInit);
+	    gridInit.push( Array.from( Array(numCols), () => 0) )
+	}
+	
+	return gridInit;
+    });
+
+    console.log(grid);
+
+    const gridDiv = () => {
+	return (
+	grid.map( row => row.map( col => (
+
+			  <div
+
+//			  key={`${i}-${k}`}
+			  style={{
+			      width: 20,
+			      height: 20,
+//			      backgroundColor: ( grid[i][k] ? "pink" : undefined ),
+			      border: "solid 1px grey" }}
+
+			  >
+			      
+			  </div>
+			      
+		  ))
+		)
+	)}
+    
+    console.log(gridDiv());
+
+    return (
+	
+	<div
+	style=
+	    {{
+	    display: "grid",
+	    gridTemplateColumns: `repeat(${numCols}, 20px)`
+	    }}
+
+	>
+	    
+	{gridDiv()}
+	
+	</div>
+
+    );
+};
 
 export default App;
